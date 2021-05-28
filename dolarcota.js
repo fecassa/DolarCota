80ab5847811df0fa347eb1a26b337407d8b2489b
Fernando Cassiano <fernando.cassiano-tec@outlook.com>
2021-05-28 17:29:40 -0300

    ajustes google chart

--- dolarcota.js
+++ dolarcota.js
@@ -1,73 +1,89 @@
 $(document).ready(function () {
 
-    var result;
-    var cotacaoD0 = {};
-
     var dataInicial = new Date();
+    var dataFinal = new Date();
 
-    var diaDaSemana = dataInicial.getDate();
+    dataInicial = new Date(dataInicial.setDate(dataInicial.getDate() - 15));
 
-    if (diaDaSemana == 6) {
-        dataInicial = dataInicial.setDate(dataInicial.getDate() - 1);
-        dataInicial = new Date(dataInicial);
-    }
-    if (diaDaSemana == 7) {
-        dataInicial = dataInicial.setDate(dataInicial.getDate() - 2);
-        dataInicial = new Date(dataInicial);
-    }
+    var dataIncialQueryString = FormatarParametroQueryString(dataInicial);
+    var dataFinalQueryString = FormatarParametroQueryString(dataFinal);
 
+    var historicoCotacoes = {};
+    var totalCotacoes;
+
+    console.log("Data inicial : " + dataIncialQueryString)
+    console.log("Data final : " + dataFinalQueryString)
+
+    var urlbcb = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?%40dataInicial=" + dataIncialQueryString + "&%40dataFinalCotacao=" + dataFinalQueryString + "&%24format=json";
+
+    console.log("URL bacen: " + urlbcb);
+
+    // Set chart options
+    var options = {
+        'titlePosition': 'none',
+        'width': 600,
+        'height': 300,
+        'legend': { position: 'none' },
+    };
 
     $.ajax({
-        url: "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?%40dataCotacao=" + FormatarParametroQueryString(dataInicial) + "&%24format=json",
+        url: urlbcb,
         contentType: "application/json",
         dataType: 'json',
         success: function (data) {
-            cotacaoD0 = data;
-            if (cotacaoD0.value == '') {
-                dataInicial = dataInicial.setDate(dataInicial.getDate() - 1);
-                dataInicial = new Date(dataInicial);
+            totalCotacoes = data.value.length;
+            historicoCotacoes = data;
+            console.log("Número de cotações no período: " + totalCotacoes);
 
-                $.ajax({
-                    url: "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?%40dataCotacao=" + FormatarParametroQueryString(dataInicial) + "&%24format=json",
-                    contentType: "application/json",
-                    dataType: 'json',
-                    success: function (data) {
-                        cotacaoD0 = data;
-                        document.getElementById("dolar").innerHTML = cotacaoD0.value[0].cotacaoVenda.toString().replace(".", ",");
-                        document.getElementById("log").innerHTML = cotacaoD0.value[0].dataHoraCotacao;
-                    }
-                })
-            }
-            else {
-                document.getElementById("dolar").innerHTML = cotacaoD0.value[0].cotacaoVenda.toString().replace(".", ",");
-                document.getElementById("log").innerHTML = "Fechamento de " + cotacaoD0.value[0].dataHoraCotacao;
-            }
+            document.getElementById("dolar").innerHTML = historicoCotacoes.value[totalCotacoes - 1].cotacaoVenda.toString().replace(".", ",");
+
+            google.charts.load('current', { 'packages': ['line'] });
+            google.charts.setOnLoadCallback(drawChart);
         }
     });
 
-    function Feriados() {
-        //Lista de feriados do site da Febraban
-        var feriados = [
-            new Date('2021-01-01'),
-            new Date('2021-02-15'),
-            new Date('2021-02-16'),
-            new Date('2021-04-02'),
-            new Date('2021-04-21'),
-            new Date('2021-05-01'),
-            new Date('2021-06-03'),
-            new Date('2021-09-07'),
-            new Date('2021-10-12'),
-            new Date('2021-11-02'),
-            new Date('2021-11-15'),
-            new Date('2021-12-25'),
-        ];
-        return feriados;
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+
+        var indiceCotacao = totalCotacoes - 10;
+        var dataCotacaoGrafico;
+        var valorCotacaoGrafico;
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Dia');
+        data.addColumn('number', 'Cotacao');
+
+        for (var contador = 1; contador <= 10; contador++) {
+            dataCotacaoGrafico = new Date(historicoCotacoes.value[indiceCotacao].dataHoraCotacao);
+            valorCotacaoGrafico = historicoCotacoes.value[indiceCotacao].cotacaoVenda.toString();
+
+            data.addRows([
+                [(dataCotacaoGrafico.getDate().toString() + "/" + (dataCotacaoGrafico.getMonth() + 1).toString()), Number(valorCotacaoGrafico)]
+            ]);
+
+            indiceCotacao = indiceCotacao + 1;
+        }
+
+        var NumberFormat = new google.visualization.NumberFormat(
+            //You can explore various type of patterns in Documentation
+            { pattern: '##.####' }
+        );
+
+        NumberFormat.format(data, 1); // Apply formatter to second column
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.charts.Line(document.getElementById('chart_div'));
+
+        chart.draw(data, google.charts.Line.convertOptions(options));
     }
 
     function FormatarParametroQueryString(data) {
-        var dia = ("0" + (dataInicial.getDate())).slice(-2);
-        var mes = ("0" + (dataInicial.getMonth() + 1)).slice(-2);
-        var ano = dataInicial.getFullYear();
+        var dia = ("0" + (data.getDate())).slice(-2);
+        var mes = ("0" + (data.getMonth() + 1)).slice(-2);
+        var ano = data.getFullYear();
 
         var dataIncialQueryString = "'" + mes + "-" + dia + "-" + ano + "'";
 
